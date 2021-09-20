ebad83c2c5022f7581973de0b6fb3ff8f9061de6
KarKeshishyan <karenkeshishyan96@gmail.com>
2021-09-20 19:22:08 +0400

    changed name tarasxal kar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Karen Keshisyan</strong>
+                <strong className="main-name"> Karen Keshishyan</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
